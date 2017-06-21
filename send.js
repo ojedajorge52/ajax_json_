d034bfd0f5db88ac937477a1cab0ade7fc1b59ac
TriquixD <guerrero_833@hotmail.com>
2017-06-21 13:28:51 -0500

    Update send.js

--- send.js
+++ send.js
@@ -25,4 +25,39 @@ $('document').ready(function(){
 			},1000)
 		})
 	})
+	$('#nuevo').submit(function(e){
+		e.preventDefault();
+		var data=$(this).serializeArray();
+		data.push({name:'tag', value:'nuevo'});
+		$.ajax({
+			url: 'process.php',
+			type: 'post',
+			dataType: 'json',
+			data: data,
+			
+		})
+		.done(function(){
+			$('#salida').attr('class','alert alert-success').text("Registrado exitosamente").fadeIn('slow').delay(1000);
+			setTimeout(function(){
+				$('#salida').fadeOut('slow').delay(1000);
+				setTimeout(function(){
+					$('input').val('').delay;
+				},1000)
+			},1000)
+			
+
+		})
+		.fail(function(){
+			$('#salida').attr('class','alert alert-danger').text("Error").fadeIn('slow').delay(1000);
+			setTimeout(function(){
+				$('#salida').fadeOut('slow').delay(1000);
+			})
+			
+		})
+		.always(function(){
+			setTimeout(function(){
+			$('.fa').hide();
+			},1000)
+		})
+	})
 })
